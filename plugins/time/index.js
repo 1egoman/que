dcba05c8ad60da5a01fa5a4fc0d6c9053836fde7
1egoman <rsg1egoman@gmail.com>
2014-08-17 17:37:52 -0400

    added date functionality

--- plugins/time/index.js
+++ plugins/time/index.js
@@ -18,20 +18,24 @@ exports.queries = [
     }
   },
   {
-    validate: /date/gi,
+    validate: function(query) {
+
+      // must match one of these
+      return query.has("day") || query.has("date")
+    },
     then: function(query, services) {
 
       // parse when this is happening
       when = services.query.whenParser(query) || Date.create()
 
       // give the date that was requested
-      if ( query.has('day') ) {
+      if ( query.has(' day ') ) {
         // ex: monday
         return "Today is a " + when.format('{Weekday}')
 
-      } else if ( query.has('day') ) {
-        // 
-        return "The time is " + when.format('{12hr}:{mm}{tt}')
+      } else if ( query.has(' date ') ) {
+        // ex: June 5 2014
+        return "Today is  " + when.format('{Month} {d}, {yyyy}')
       }
 
 
