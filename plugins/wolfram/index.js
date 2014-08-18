db0a4c19632575b71fcc6c9fa1a5672ef5ace26c
1egoman <rsg1egoman@gmail.com>
2014-08-18 09:39:23 -0400

    wolfram alpha support

---
+++ plugins/wolfram/index.js
@@ -0,0 +1,52 @@
+var wolfram = require('wolfram')
+
+exports.queries = [
+  {
+    validate: /.*/gi,
+    priority: -Infinity, // always last,
+    wolframKey: "4W736E-Y82E88RJYQ", // the auth key
+    then: function(query, services, callback) {
+
+      client = wolfram.createClient(this.wolframKey)
+
+      client.query(query, function(err, result) {
+        if (err) throw err;
+
+        if (!result) callback(false)
+
+        // get response pod
+        primary = result.find(function(item){
+          return item.title == 'Response' || !item.title.has('Input')
+        })
+
+        queryText = primary.subpods[0].value.replace('\n', ';;')
+
+        // do some formatting
+        if ( queryText.has('|') ) {
+
+          // split the query
+          splitQuery = queryText.split(";;")
+          splitQuery.each(function(i, ct){
+
+            // get key and value
+            key = i.split('|')[0]
+            value = i.split('|')[1]
+
+            // format it a little better
+            splitQuery[ct] = "The " + key + " is " + value + ". "
+          })
+
+          queryText = splitQuery.join('')
+        }
+
+        callback({
+          OK: queryText,
+          image: primary.subpods[0].image || null
+        })
+      })
+
+    }
+  }
+]
+
+exports.services = {}
