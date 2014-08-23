2462de9548bc02b26baffce8b4b9582e64bc9b59
1egoman <rsg1egoman@gmail.com>
2014-08-23 12:23:24 -0400

    devices deletion

--- plugins/devices/index.js
+++ plugins/devices/index.js
@@ -3,15 +3,32 @@ fs = require('fs');
 
 exports.queries = [
   {
-    validate: /add device .*;.*;.*/gi,
+    validate: /add device .*;.*;(.* ?)*/gi,
     then: function(query, services, callback) {
       // get parts
+      console.log(1)
       parts = query.substr(11).split(';')
 
       // add to service
       all = new services.devices(services).getDevices()
       all.push({name: parts[0], ip: parts[1], tags: parts[2].split(' ')})
       new services.devices(services).putDevices(all)
+
+      // return success
+      callback("OK")
+    }
+  },
+  {
+    validate: /del device .*/gi,
+    then: function(query, services, callback) {
+      // get tags
+      tags = query.substr(11).split(' ')
+
+      // add to service
+      devices = new services.devices(services)
+      d = devices.getDevices()
+      d.remove( devices.findByTag(tags[0])[0] )
+      devices.putDevices(d)
 
       // return success
       callback("OK")
@@ -34,8 +51,16 @@ exports.services = {
       return new services.persist("devices").put({devices: devices});
     }
 
+    // get list of devices
     this.getData = function() {
       return {devices: this.getDevices()}
+    }
+
+    // look for devices that are tagged with the specified tag
+    this.findByTag = function(tag) {
+      return this.getDevices().filter(function(device){
+        return device.tags.indexOf(tag) !== -1
+      })
     }
 
   }
